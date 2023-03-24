7a5b40e0d4ca2ab701e47014dd2d22fad41e25c4
aliseyedi <solafiali657@gmail.com>
2023-03-25 00:05:07 +0430

    feat ( component ) : add Discover & SongCard Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import { Navbar } from "./components";
+import Discover from "./pages/Discover";
 
 function App() {
   return (
-    <div className="App h-screen w-full bg-orange-400 dark:bg-blue-600">
+    <div className="App h-screen w-full bg-white dark:bg-blue-600">
       <Navbar />
       <h2 className=" text-3xl font-bold ">music app react</h2>
+      <Discover />
     </div>
   );
 }
---
+++ src/pages/Discover.jsx
@@ -0,0 +1,11 @@
+import SongCard from "../components/SongCard";
+
+export default function Discover() {
+  return (
+    <div>
+      <div className="flex flex-wrap justify-center gap-8 p-3 sm:justify-start">
+        <SongCard />
+      </div>
+    </div>
+  );
+}
