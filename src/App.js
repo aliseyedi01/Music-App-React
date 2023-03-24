f7bbeb4cb3e4951c827e04d4980baa67395f650e
aliseyedi <solafiali657@gmail.com>
2023-03-24 20:09:09 +0430

    feat(style) : add tailwindCss to project

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h2>music app react</h2>
+      <h2 className="text-3xl font-bold underline bg-red-300">music app react</h2>
     </div>
   );
 }
