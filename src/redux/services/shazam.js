6e43b72a02ea15e29d573cfd51afbe3f3738b788
aliseyedi <solafiali657@gmail.com>
2023-03-27 02:02:37 +0430

    feat  ( env ) : add environment variable for API Key

--- src/redux/services/shazam.js
+++ src/redux/services/shazam.js
@@ -1,15 +1,13 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-const apiKey = process.env.REACT_APP_API_KEY;
+const apiKey = process.env.REACT_APP_API_KEY_TWO;
+console.log(apiKey);
 
 export const shazamApi = createApi({
   reducerPath: "shazamApi",
   baseQuery: fetchBaseQuery({
     baseUrl: "https://shazam.p.rapidapi.com/",
     prepareHeaders: (headers) => {
-      headers.set(
-        "X-RapidAPI-Key",
-        "68e42016c1msh5080d0e469e0b0cp1d7c07jsn539b970c36c9"
-      );
+      headers.set("X-RapidAPI-Key", apiKey);
       return headers;
     },
   }),
