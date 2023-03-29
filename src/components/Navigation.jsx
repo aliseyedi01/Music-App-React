ad80cf244f1fc3e42878a0bc715b954d20f723f6
aliseyedi <solafiali657@gmail.com>
2023-03-29 18:00:34 +0430

    feat ( style ) : add react-tooltip library and apply on navigation component

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,23 +1,30 @@
 import React from "react";
 import { HiArrowCircleLeft, HiArrowCircleRight } from "react-icons/hi";
-
+import { Tooltip } from "react-tooltip";
 import { useNavigate } from "react-router-dom";
 
 export default function Navigate() {
   const navigate = useNavigate();
   return (
     <div className="flex items-center justify-center gap-1   p-2 ">
+      <Tooltip
+        id="back"
+        place="bottom"
+        className="z-40 !transform !rounded-xl !bg-red-600 font-Ubuntu font-bold  !text-white !transition-all !duration-300 hover:bg-yellow-400"
+      />
       <button
         className=" cursor-pointer  text-3xl font-bold text-light_txt_Main dark:text-dark_txt_Main "
         onClick={() => navigate(-1)}
-        title="Back"
+        data-tooltip-id="back"
+        data-tooltip-content="Back"
       >
         <HiArrowCircleLeft />
       </button>
       <button
         className="cursor-pointer text-3xl font-bold  text-light_txt_Main  dark:text-dark_txt_Main "
         onClick={() => navigate(+1)}
-        title="Forward"
+        data-tooltip-id="back"
+        data-tooltip-content="Forward"
       >
         <HiArrowCircleRight />
       </button>
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 import { BrowserRouter } from "react-router-dom";
+import "react-tooltip/dist/react-tooltip.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
