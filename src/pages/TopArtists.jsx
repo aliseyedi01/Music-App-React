21644f029f8676e7d277571878dad9bfdb93591b
aliseyedi <solafiali657@gmail.com>
2023-03-29 20:54:47 +0430

    feat ( animation ) : add animation to image TopArtists component

--- src/pages/TopArtists.jsx
+++ src/pages/TopArtists.jsx
@@ -12,19 +12,24 @@ export default function TopArtists() {
   return (
     <div className="  flex h-full w-full flex-col   bg-light_bg_Main py-3 dark:bg-dark_bg_Main">
       <div className="flex items-center justify-between px-4">
-        <h2 className="font-Montserrat text-2xl text-white">Top Artist</h2>
+        <h2 className="font-Montserrat text-2xl text-light_txt_Main dark:text-dark_txt_Main ">
+          Top Artist
+        </h2>
         <Navigation />
       </div>
       <div className="hide-scrollbar grid h-full w-full grid-cols-3 justify-center justify-items-center overflow-y-scroll ">
         {data?.tracks?.map((artist, i) => (
           <Link to={`/artists/${artist?.artists[0].adamid}`}>
             {/* <Link to="/artists/"> */}
-            <div key={i} className=" m-4 h-36 w-36">
-              <img
-                src={artist.images.background}
-                className="h-32 w-32 transform overflow-hidden rounded-full opacity-70 shadow-md transition-all duration-150 hover:opacity-100 hover:shadow-amber-300"
-              />
-              <p className=" mt-1 truncate text-center font-Montserrat text-sm text-red-400">
+            <div key={i} className=" m-4 h-36 w-36 ">
+              <div className="h-32 w-32 overflow-hidden rounded-full">
+                <img
+                  src={artist.images.background}
+                  className="transition-ease-in h-32 w-32 transform overflow-hidden rounded-full shadow-md transition-all    duration-700 hover:scale-125   "
+                />
+              </div>
+
+              <p className=" mt-1 truncate text-center font-Montserrat text-sm font-semibold text-light_txt_Main dark:text-dark_txt_Main">
                 {artist.subtitle}
               </p>
             </div>
