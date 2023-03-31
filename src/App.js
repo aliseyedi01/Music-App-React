e4e73629b6adf5714cfad5e3bbb69fa86426625a
aliseyedi <solafiali657@gmail.com>
2023-03-31 16:48:15 +0430

    feat(lazy-loading): Add lazy loading for improved performance

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import { SongDetails, Sidebar, MusicPlayer, Navigate } from "./components";
+import { SongDetails, Sidebar, MusicPlayer, Loading } from "./components";
 import { Route, Routes } from "react-router-dom";
+import { lazy, Suspense } from "react";
 
-import { Discover, TopArtists, ArtistDetail, Search, Home } from "./pages";
+const Home = lazy(() => import("./pages/Home"));
+const TopArtists = lazy(() => import("./pages/TopArtists"));
+const Discover = lazy(() => import("./pages/Discover"));
+const Search = lazy(() => import("./pages/Search"));
+const ArtistDetail = lazy(() => import("./pages/ArtistDetail"));
 
 function App() {
   return (
@@ -9,16 +14,60 @@ function App() {
       <div className="h-full w-[12%]">
         <Sidebar />
       </div>
-      <div className="hide-scroll hide-scrollbar  w-[60%] overflow-y-scroll">
+
+      <div className="hide-scroll hide-scrollbar  w-[60%] overflow-y-scroll ">
         <Routes>
-          <Route path="*" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/discover" element={<Discover />} />
-          <Route path="/search" element={<Search />} />
-          <Route path="/top-artists" element={<TopArtists />} />
-          <Route path="/artists/:artistsId" element={<ArtistDetail />} />
+          <Route
+            path="*"
+            element={
+              <Suspense fallback={<Loading />}>
+                <Home />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/home"
+            element={
+              <Suspense fallback={<Loading />}>
+                <Home />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/discover"
+            element={
+              <Suspense fallback={<Loading />}>
+                <Discover />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/search"
+            element={
+              <Suspense fallback={<Loading />}>
+                <Search />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/top-artists"
+            element={
+              <Suspense fallback={<Loading />}>
+                <TopArtists />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/artists/:artistsId"
+            element={
+              <Suspense fallback={<Loading />}>
+                <ArtistDetail />
+              </Suspense>
+            }
+          />
         </Routes>
       </div>
+
       <div className="h-full w-[28%] overflow-y-hidden bg-light_bg_Side dark:bg-dark_bg_Side ">
         <MusicPlayer />
         <SongDetails />
