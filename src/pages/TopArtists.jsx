90a30bf82d952487aa75bd4c01c399afa7391634
aliseyedi <solafiali657@gmail.com>
2023-04-02 13:59:22 +0430

    feat ( loading-image ) : add loading image to TopArtists component

--- src/pages/TopArtists.jsx
+++ src/pages/TopArtists.jsx
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { Navigation } from "../components";
-
 import { useGetSongTrackQuery } from "../redux/services/shazam";
 
 export default function TopArtists() {
-  const { data, isFetching, error, isLoading } = useGetSongTrackQuery();
-  // console.log(data.tracks);
+  const { data } = useGetSongTrackQuery();
 
-  // console.log("isFetching", isFetching);
-  // console.log("error", error);
-  // console.log("isLoading ", isLoading);
+  const [imageLoadingStates, setImageLoadingStates] = useState(
+    data?.tracks ? Object.fromEntries(data.tracks.map((artist, i) => [i, true])) : {}
+  );
+
+  useEffect(() => {
+    if (data?.tracks) {
+      setImageLoadingStates((prevState) => {
+        const newStates = Object.fromEntries(data.tracks.map((artist, i) => [i, true]));
+        return { ...prevState, ...newStates };
+      });
+    }
+  }, [data]);
+
+  const handleImageLoad = (index) => {
+    setImageLoadingStates((prevStates) => ({
+      ...prevStates,
+      [index]: false,
+    }));
+  };
+
+  console.log("Loading", imageLoadingStates);
 
   return (
     <div className="  flex h-full w-full flex-col   bg-light_bg_Main py-3 dark:bg-dark_bg_Main">
@@ -22,21 +38,33 @@ export default function TopArtists() {
       </div>
       <div className="hide-scrollbar grid h-full w-full grid-cols-3 justify-center justify-items-center overflow-y-scroll ">
         {data?.tracks?.map((artist, i) => (
-          <Link to={`/artists/${artist?.artists[0].adamid}`}>
-            {/* <Link to="/artists/"> */}
-            <div key={i} className=" m-4 h-36 w-36 ">
-              <div className="h-32 w-32 overflow-hidden rounded-full ">
-                {isFetching && isLoading ? (
-                  <div className="h-32 w-32 animate-pulse bg-slate-700 "></div>
-                ) : (
-                  <img
-                    src={artist.images.background}
-                    className="transition-ease-in h-32 w-32 transform overflow-hidden rounded-full shadow-md transition-all    duration-700 hover:scale-125   "
-                  />
-                )}
+          <Link key={i} to={`/artists/${artist?.artists[0].adamid}`}>
+            <div className="  h- flex w-36 flex-col items-center  justify-center">
+              {imageLoadingStates[i] && (
+                <div className=" h-32 w-32 animate-pulse rounded-full bg-blue-700 dark:bg-slate-600"></div>
+              )}
+              <div
+                className={
+                  imageLoadingStates[i] ? "hidden" : "h-32 w-32 overflow-hidden  rounded-full  "
+                }
+              >
+                <img
+                  src={artist.images.background}
+                  alt="artists"
+                  loading="eager"
+                  className={
+                    imageLoadingStates[i]
+                      ? "hidden"
+                      : "h-32 w-32 transform overflow-hidden rounded-full shadow-md transition-transform duration-700 hover:scale-125"
+                  }
+                  onLoad={() => handleImageLoad(i)}
+                  onError={() =>
+                    setImageLoadingStates((prevStates) => ({ ...prevStates, [i]: true }))
+                  }
+                />
               </div>
 
-              <p className=" mt-1 truncate text-center font-Montserrat text-sm font-semibold text-light_txt_Main dark:text-dark_txt_Main">
+              <p className=" mt-1 w-32  truncate text-center font-Montserrat text-sm font-semibold text-light_txt_Main dark:text-dark_txt_Main">
                 {artist.subtitle}
               </p>
             </div>
