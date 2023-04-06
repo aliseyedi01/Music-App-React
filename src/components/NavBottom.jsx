4c255bdf6e4aba1363e551f503cd3f7ecb72fd3a
aliseyedi <solafiali657@gmail.com>
2023-04-07 00:30:25 +0430

    style ( NavBottom ) : increase size Icon and height section

--- src/components/NavBottom.jsx
+++ src/components/NavBottom.jsx
@@ -10,28 +10,28 @@ export default function NavBottom() {
     setDarkMode(!darkMode);
   };
   return (
-    <div className=" text-grey-200 absolute bottom-0 left-0  z-50  block h-10 w-full bg-opacity-30 text-xl text-light_bg_Side  backdrop-blur-2xl md:hidden">
-      <div className="flex h-9 w-full items-center justify-between bg-amber-900 bg-opacity-40 px-2 text-orange-300 backdrop-blur-xl  dark:bg-indigo-400 dark:bg-opacity-30  dark:text-orange-500 ">
+    <div className=" text-grey-200 absolute bottom-0 left-0  z-50  block h-11 w-full bg-opacity-30 text-xl text-light_bg_Side  backdrop-blur-2xl md:hidden">
+      <div className="flex h-11 w-full items-center justify-between bg-amber-900 bg-opacity-40 px-3 text-orange-300 backdrop-blur-xl  dark:bg-indigo-400 dark:bg-opacity-30  dark:text-orange-500 ">
         <NavLink to="/">
-          <HiHome className="" />
+          <HiHome className="text-xl" />
         </NavLink>
         <NavLink to="/discover">
-          <HiViewGrid className="" />
+          <HiViewGrid className="text-xl" />
         </NavLink>
         <NavLink to="/search">
-          <HiSearchCircle className="" />
+          <HiSearchCircle className="text-xl" />
         </NavLink>
         <NavLink to="/top-artists">
-          <HiUserGroup className="" />
+          <HiUserGroup className="text-xl" />
         </NavLink>
         <button
           className="flex w-7 transform   border-yellow-700 p-1 text-light_txt_Main outline-none transition-all duration-150  dark:border-white dark:text-dark_txt_Main   "
           onClick={handleThemeChange}
         >
           {!darkMode ? (
-            <HiSun className="cursor-pointer text-2xl text-yellow-300" />
+            <HiSun className="cursor-pointer text-3xl text-yellow-300" />
           ) : (
-            <HiMoon className="cursor-pointer text-2xl text-white" />
+            <HiMoon className="cursor-pointer text-3xl text-white" />
           )}
         </button>
       </div>
